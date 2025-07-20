4519a9e40468b6bce100366e632487c1de28c40d
pastorsheggs <bamidel692@gmail.com>
2025-07-21 05:29:23 +0800

    Update models.py

--- alx_travel_app/alx_travel_app/listings/models.py
+++ alx_travel_app/alx_travel_app/listings/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class Listing(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    location = models.CharField(max_length=255)
+    price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
+    max_guests = models.IntegerField()
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Booking(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    guest_name = models.CharField(max_length=255)
+    check_in = models.DateField()
+    check_out = models.DateField()
+    guests = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.guest_name} booking {self.listing.title}"
+
+
+class Review(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    reviewer_name = models.CharField(max_length=255)
+    rating = models.IntegerField()
+    comment = models.TextField()
+
+    def __str__(self):
+        return f"Review for {self.listing.title} by {self.reviewer_name}"
